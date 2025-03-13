ef9c2b7e3d402a0fc2f6296a9819fe5ea1c9c8ea
MarioSpore <ironmariospore@gmail.com>
2025-03-13 17:56:10 -0400

    initial commit
    Currently adds facilties and battle music

---
+++ overwrites.gd
@@ -0,0 +1,27 @@
+extends Node
+
+func _init():
+# music
+	var bossbot1 = preload("res://mods/MarioSpore-TTCCpack/mods-unpacked/MarioSpore-TTCCpack/overwrites/audio/music/BB_club_silver.ogg")
+	bossbot1.take_over_path("res://audio/music/Bossbot_Factory_v1.ogg")
+	var bossbot2 = preload("res://mods/MarioSpore-TTCCpack/mods-unpacked/MarioSpore-TTCCpack/overwrites/audio/music/BB_club_gold.ogg")
+	bossbot2.take_over_path("res://audio/music/Bossbot_Factory_v2.ogg")
+	var bossbot3 = preload("res://mods/MarioSpore-TTCCpack/mods-unpacked/MarioSpore-TTCCpack/overwrites/audio/music/BB_club_diamond.ogg")
+	bossbot3.take_over_path("res://audio/music/Bossbot_Factory_v3.ogg")
+	var bossbotceo = preload("res://mods/MarioSpore-TTCCpack/mods-unpacked/MarioSpore-TTCCpack/overwrites/audio/music/BB_club_gold_encntr.ogg")
+	bossbotceo.take_over_path("res://audio/music/BossBot_CEO_v1.ogg")
+	
+	var daoffice = preload("res://mods/MarioSpore-TTCCpack/mods-unpacked/MarioSpore-TTCCpack/overwrites/audio/music/LB_office_c.ogg")
+	daoffice.take_over_path("res://audio/music/DA_Office.ogg")
+	var jurybg = preload("res://mods/MarioSpore-TTCCpack/mods-unpacked/MarioSpore-TTCCpack/overwrites/audio/music/LB_office_encntr_c_virtual.ogg")
+	jurybg.take_over_path("res://audio/music/LB_juryBG.ogg")
+	
+	var installer = preload("res://mods/MarioSpore-TTCCpack/mods-unpacked/MarioSpore-TTCCpack/overwrites/audio/music/CB_mint_bullion.ogg")
+	installer.take_over_path("res://audio/music/installer.ogg")
+	var suit_winning = preload("res://mods/MarioSpore-TTCCpack/mods-unpacked/MarioSpore-TTCCpack/overwrites/audio/music/CB_mint_encntr_bullion.ogg")
+	suit_winning.take_over_path("res://audio/music/encntr_suit_winning.ogg")
+	
+	var sbfactory = preload("res://mods/MarioSpore-TTCCpack/mods-unpacked/MarioSpore-TTCCpack/overwrites/audio/music/SB_factory.ogg")
+	installer.take_over_path("res://audio/music/CHQ_FACT_bg.ogg")
+	var sbfactory_encntr = preload("res://mods/MarioSpore-TTCCpack/mods-unpacked/MarioSpore-TTCCpack/overwrites/audio/music/SB_factory_encntr.ogg")
+	suit_winning.take_over_path("res://audio/music/encntr_general_bg.ogg")
