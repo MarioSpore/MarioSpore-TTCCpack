2195cbb0e37da5d3bd8cbd981d7d7e77b406e317
MarioSpore <ironmariospore@gmail.com>
2025-05-03 20:00:15 -0400

    Rewritten code for v1.1 update
    A LOT of things.
    
    - Added randomization to facilities & battles, thanks to Evan
    - Added a variety of new tracks such as main theme, "MS_Pairing" and others that should hopefully working that usually would not work otherwise.
    - Overwrites & custom_music is now essentially obsolete.

--- mod_main.gd
+++ mod_main.gd
@@ -1,40 +1,95 @@
 extends Node
 
+## CHANGE THESE FOR YOUR MOD!!!!!
+const ROOM_LOADER_DIR := "MarioSpore-TTCCpack"
+const ROOM_LOADER_LOG := "MarioSpore-TTCCpack:Main"
 
-const MOD_DIR := "MarioSpore-TTCCpack"
-const LOG_NAME := "MarioSpore-TTCCpack:Main"
+
+const MUSIC_MAPPINGS : Dictionary[String, String] = {
+	"climb_boss.ogg" : "GS_Race_SS.ogg",
+	"clown_boss.ogg" : "Flock_of_one.ogg",
+	"ground_floor_investor.ogg" : "instance_rainmaker_battle_empty.ogg",
+	"encntr_skull_master.ogg" : "LB_boss_cutscene_1.ogg",
+	"new_lavaboss_battle.ogg" : "Instance_rainmaker_battle_empty.ogg",
+	"main_theme.ogg" : "tt_theme.ogg",
+	"MG_Pairing.ogg" : "MG_Pairing.ogg",
+	#"beta_installer.ogg" : "building_c_elevator.ogg",
+	#"beta_installer.ogg" : "building_g_elevator.ogg",
+	#"beta_installer.ogg" : "building_l_elevator.ogg",
+	#"beta_installer.ogg" : "building_m_elevator.ogg",
+	#"beta_installer.ogg" : "building_s_elevator.ogg",
+	
+}
+const RANDOM_FACTORY_MUSIC: Dictionary[String, String] = {
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/SB_factory.ogg" : "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/SB_factory_encntr.ogg",
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/SB_factory_side.ogg" : "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/SB_factory_encntr_side.ogg",
+}
+const RANDOM_MINT_MUSIC: Dictionary[String, String] = {
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/CB_mint_coin.ogg" : "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/CB_mint_encntr_coin.ogg",
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/CB_mint_dollar.ogg": "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/CB_mint_encntr_dollar.ogg",
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/CB_mint_bullion.ogg" : "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/CB_mint_encntr_bullion.ogg",
+}
+const RANDOM_OFFICE_MUSIC: Dictionary[String, String] = {
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/LB_office_a.ogg" : "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/LB_office_encntr_a.ogg",
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/LB_office_b.ogg" : "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/LB_office_encntr_b.ogg",
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/LB_office_c.ogg" : "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/LB_office_encntr_c.ogg",
+}
+const RANDOM_CLUB_MUSIC: Dictionary[String, String] = {
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/BB_club_silver.ogg" : "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/BB_club_silver_encntr.ogg",
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/BB_club_gold.ogg" : "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/BB_club_gold_encntr.ogg",
+	"res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/BB_club_diamond.ogg" : "res://mods-unpacked/MarioSpore-TTCCpack/music_tracks/BB_club_diamond_encntr.ogg",
+}
 
 var mod_dir_path := ""
-var extensions_dir_path := ""
-var translations_dir_path := ""
 
 
 func _init() -> void:
-	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(MOD_DIR)
-	# Add extensions
-	install_script_extensions()
-	install_script_hook_files()
+	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(ROOM_LOADER_LOG)
 
-	# Add translations
-	add_translations()
-
-
-func install_script_extensions() -> void:
-	extensions_dir_path = mod_dir_path.path_join("extensions")
-
-
-func install_script_hook_files() -> void:
-	extensions_dir_path = mod_dir_path.path_join("extensions")
-
-
-func add_translations() -> void:
-	translations_dir_path = mod_dir_path.path_join("translations")
-
-# mod_main.gd
 func _ready() -> void:
-	var s_script = load(mod_dir_path.path_join("custom_music.gd"))
-	if s_script:
-		var s = s_script.new()
-		add_child(s)
-		
-	ModLoaderLog.info("Ready", LOG_NAME)
+	AudioManager.s_music_changed.connect(on_music_changed)
+	ModLoaderLog.info("Ready!", ROOM_LOADER_LOG)
+	Util.s_floor_started.connect(on_floor_started)
+
+func on_music_changed(new_music : AudioStream, _is_default) -> void:
+	if not MUSIC_MAPPINGS.keys().find(new_music.resource_path.get_file()) == -1:
+		AudioManager.set_music(get_music_file(MUSIC_MAPPINGS[new_music.resource_path.get_file()]))
+
+func get_music_file(file_name : String) -> AudioStream:
+	return load(ModLoaderMod.get_unpacked_dir().path_join(ROOM_LOADER_DIR + "/music_tracks/" + file_name))
+
+func on_floor_started(game_floor: GameFloor) -> void:
+	var floor_variant: FloorVariant = game_floor.floor_variant
+	floor_variant.floor_type = floor_variant.floor_type.duplicate()
+	if floor_variant.floor_name.to_lower().contains("factory"):
+		on_factory_entered(floor_variant)
+	if floor_variant.floor_name.to_lower().contains("mint"):
+		on_mint_entered(floor_variant)
+	if floor_variant.floor_name.to_lower().contains("office"):
+		on_office_entered(floor_variant)
+	if floor_variant.floor_name.to_lower().contains("club"):
+		on_club_entered(floor_variant)
+
+func on_factory_entered(floor_variant: FloorVariant) -> void:
+	var factory_facility_music: String = RandomService.array_pick_random('true_random', RANDOM_FACTORY_MUSIC.keys())
+	var factory_battle_music: String = RANDOM_FACTORY_MUSIC[factory_facility_music]
+	floor_variant.floor_type.background_music = [load(factory_facility_music)]
+	floor_variant.floor_type.battle_music = load(factory_battle_music)
+
+func on_mint_entered(floor_variant: FloorVariant) -> void:
+	var mint_facility_music: String = RandomService.array_pick_random('true_random', RANDOM_MINT_MUSIC.keys())
+	var mint_battle_music: String = RANDOM_MINT_MUSIC[mint_facility_music]
+	floor_variant.floor_type.background_music = [load(mint_facility_music)]
+	floor_variant.floor_type.battle_music = load(mint_battle_music)
+	
+func on_office_entered(floor_variant: FloorVariant) -> void:
+	var office_facility_music: String = RandomService.array_pick_random('true_random', RANDOM_OFFICE_MUSIC.keys())
+	var office_battle_music: String = RANDOM_OFFICE_MUSIC[office_facility_music]
+	floor_variant.floor_type.background_music = [load(office_facility_music)]
+	floor_variant.floor_type.battle_music = load(office_battle_music)
+	
+func on_club_entered(floor_variant: FloorVariant) -> void:
+	var club_facility_music: String = RandomService.array_pick_random('true_random', RANDOM_CLUB_MUSIC.keys())
+	var club_battle_music: String = RANDOM_OFFICE_MUSIC[club_facility_music]
+	floor_variant.floor_type.background_music = [load(club_facility_music)]
+	floor_variant.floor_type.battle_music = load(club_battle_music)
